ccf33ad0a8fd82f67a9d3391d8d722a9be292858
elmiar <35724936+Elmiar0642@users.noreply.github.com>
2026-06-09 16:29:03 +0530

    Add CrownFall engine scaffold

---
+++ src/movegen.h
@@ -0,0 +1,8 @@
+#ifndef CROWNFALL_MOVEGEN_H
+#define CROWNFALL_MOVEGEN_H
+
+#include "engine.h"
+
+int cf_movegen_for_piece(CfGame *game, const CfPiece *piece, CfMove *moves, int max_moves);
+
+#endif
