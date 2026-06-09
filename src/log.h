c299eccd7d1964a2db06571d9243cc262d4cd4b7
elmiar <35724936+Elmiar0642@users.noreply.github.com>
2026-06-09 16:24:39 +0530

    Add CrownFall engine scaffold

---
+++ src/log.h
@@ -0,0 +1,13 @@
+#ifndef CROWNFALL_LOG_H
+#define CROWNFALL_LOG_H
+
+#include "engine.h"
+
+#include <stdbool.h>
+
+bool cf_log_open(CfGame *game);
+void cf_log_event(CfGame *game, const char *event, const char *json_fields);
+void cf_log_close(CfGame *game);
+void cf_json_escape(const char *src, char *dst, int dst_size);
+
+#endif
